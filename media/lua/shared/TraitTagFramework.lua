3d034661a20d898b080087f4accc32b7baf6bddd
Badonn <badonnthedeer@protonmail.com>
2023-05-23 19:55:36 -0400

    Reorder code, get getTagsStatistics() working

--- media/lua/shared/TraitTagFramework.lua
+++ media/lua/shared/TraitTagFramework.lua
@@ -6,91 +6,139 @@ local function sanitizeTags(tagString)
     return workString;
 end
 
---Uses alphanumberic symbols and commas only.
-function TraitTags:add(traitName, tags)
-    local sanitizedTags = sanitizeTags(tags);
-    local tagTable = {};
-    --local prevComma = 0;
-    --local currChar = '';
-    if TraitFactory:getTrait(traitName)
-    then
-        if string.len(sanitizedTags) > 0
-        then
-            print("Trait Tags: inserting table entry "..traitName.." to TraitTags.");
-            table.insert(TraitTags, traitName);
-            print("Trait Tags: adding tags "..sanitizedTags.." to "..traitName.." entry.");
-            for i = 1, string.len(sanitizedTags)
-            do
-                --[[currChar = sanitizedTags:sub(i,i)
-                if currChar == ','
-                then
-                    table.insert(tagTable, sanitizedTags:sub(prevComma + 1, i));
-                    prevComma = i;
-                elseif i >= string.len(sanitizedTags)
-                then
-                    table.insert(tagTable, sanitizedTags:sub(prevComma + 1, i));
-                end]]
-                table.insert(string.split(sanitizedTags, ','));
-            end
-            table.insert(traitName, tagTable);
-        else
-            print("Trait Tags: No tags detected for "..traitName..", skipping...");
-        end
-    else
-        print("Trait Tags: Cannot find trait "..traitName..", skipping...");
-    end
-end
-
-function TraitTags:remove(traitName)
-    if TraitTags[traitName]
-    then
-        table.remove(TraitTags, traitName)
-    else
-        print("Trait Tags: No entry found for "..traitName..", cannot remove. Skipping...")    
-    end
-end
-
-function TraitTags:getTagTable(traitName)
-    if TraitTags[traitName]
-    then
-        return TraitTags[traitName]
-    else
-        print("Trait Tags: No entry found for "..traitName..", cannot get tag table. Skipping...")    
-    end
-end
 
 function TraitTags:toString()
     local returnString = "";
 
-    for trait, tagTable in ipairs(TraitTags) 
+    for trait, tagTable in pairs(TraitTags)
     do
-        returnString = returnString..trait..": "
-        --[[for _, tag in ipairs(tagTable)
-        do
-            returnString = returnString..tag..", "
-        end]]
-        returnString = returnString..table.concat(tagTable, ',');
-        --returnString = returnString:sub(1, string.len(returnString) - 2);
-        returnString = returnString.."; "
+        if type(tagTable) == "table"
+        then
+            returnString = returnString..trait..": ";
+            returnString = returnString..table.concat(tagTable, ',');
+            returnString = returnString.."; ";
+        end
     end
 
     return returnString;
 end
 
+
 function TraitTags:tagTableToString(traitName)
     local returnString = "";
 
-    if TraitTags[traitName]
+    if TraitTags[traitName] ~= nil
     then
-       --[[ for _, tag in ipairs(TraitTags[traitName])
-        do
-            returnString = returnString..tag..", "
-        end
-        returnString = returnString:sub(1, string.len(returnString) - 2);
-        return returnString;]]
         returnString = table.concat(TraitTags[traitName], ',');
     else
-        print("Trait Tags: No entry found for "..traitName..", cannot stringify tag table. Skipping...")    
+        print("Trait Tag Framework: No entry found for "..traitName..", cannot stringify tag table. Skipping...")
     end
 end
 
+
+--Uses alphanumberic symbols and commas only.
+function TraitTags:add(traitName, tags)
+    local sanitizedTags = sanitizeTags(tags);
+    local tagTable = {};
+    if TraitFactory.getTrait(traitName)
+    then
+        if TraitTags[traitName] == nil
+        then
+            print("Trait Tag Framework: Initializing entry for "..traitName.." in TraitTags")
+            TraitTags[traitName] = {};
+        end
+        if string.len(sanitizedTags) > 0
+        then
+            tagTable =  string.split(sanitizedTags, ',');
+            print("Trait Tag Framework: adding tags "..sanitizedTags.." to "..traitName.." entry.");
+            TraitTags[traitName] = tagTable;
+        else
+            print("Trait Tag Framework: No tags detected for "..traitName..", skipping...");
+        end
+    else
+        print("Trait Tag Framework: Cannot find trait "..traitName..", skipping...");
+    end
+end
+
+
+function TraitTags:remove(traitName)
+    if TraitTags[traitName] ~= nil
+    then
+        table.remove(TraitTags, traitName)
+    else
+        print("Trait Tag Framework: No entry found for "..traitName..", cannot remove. Skipping...")
+    end
+end
+
+
+function TraitTags:getTraitTags()
+    return TraitTags;
+end
+
+
+function TraitTags:getTagTable(traitName)
+    if TraitTags[traitName] ~= nil
+    then
+        return TraitTags[traitName]
+    else
+        print("Trait Tag Framework: No entry found for "..traitName..", cannot get tag table. Skipping...")
+    end
+end
+
+
+function TraitTags:getPlayerTraitTags(player)
+    local playerTraits = player:getTraits();
+    local concatenatedTags = "";
+    local returnString = "";
+    if playerTraits ~= nil
+    then
+        for _, trait in ipairs(playerTraits)
+        do
+            print(_..", "..trait);
+            if TraitTags[trait] ~= nil
+            then
+                concatenatedTags = table.concat(TraitTags[trait], ',');
+                returnString = returnString..concatenatedTags..concatenatedTags;
+            end
+        end
+    end
+    return returnString;
+end
+
+function TraitTags:getTagsStatistics()
+    local tempTable = {};
+    local sortTable = {};
+    local returnString = "";
+
+    --get
+    for trait, tagTable in pairs(TraitTags)
+    do
+        if type(tagTable) == "table"
+        then
+            for _, tag in ipairs(tagTable)
+            do
+                if tempTable[tag] ~= nil
+                then
+                    tempTable[tag] = tempTable[tag] + 1;
+                else
+                    tempTable[tag] = 1
+                end
+            end
+        end
+    end
+
+    --sort
+    for key, value in pairs(tempTable) do
+        table.insert(sortTable, {tag = key, count = value})
+    end
+    table.sort(sortTable, function(a, b) return a.count > b.count end);
+
+    --condense (limitation: cannot make a sorted table in vanilla lua. Return as string instead.
+
+    for i = 1, getn(sortTable)
+    do
+        returnString = returnString..sortTable[i].tag..": "..sortTable[i].count.."; "
+    end
+
+    return returnString;
+end
