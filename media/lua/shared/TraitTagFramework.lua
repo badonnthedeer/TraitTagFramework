51e96b2a681ce97bd9e14ec743b3af8aa7751940
Badonn <badonnthedeer@protonmail.com>
2023-05-31 18:02:56 -0400

    tagTableToString() actually returns a value. Fix getPLayerTraitTags(), add getPlayerTagStatistics(), PlayerTagLargerCountThan()

--- media/lua/shared/TraitTagFramework.lua
+++ media/lua/shared/TraitTagFramework.lua
@@ -33,6 +33,7 @@ function TraitTags:tagTableToString(traitName)
     else
         print("Trait Tag Framework: No entry found for "..traitName..", cannot stringify tag table. Skipping...")
     end
+    return returnString;
 end
 
 
@@ -86,39 +87,7 @@ function TraitTags:getTagTable(traitName)
 end
 
 
-function TraitTags:getPlayerTraitTags(player)
-    local playerTraits = player:getTraits();
-    local concatenatedTags = "";
-    local returnString = "";
-    local traitName = "";
-    --
-    --local test1 = getn(playerTraits);
-    --print(tostring(test1));
-    --
-    if playerTraits ~= nil
-    then
-        for i = 1, playerTraits:size() -1
-        do
-            local trait = playerTraits:get(i);
-
-            if TraitTags[trait] ~= nil
-            then
-                concatenatedTags = "";
-                for _,tag in pairs(TraitTags[trait])
-                do
-                    if not returnString:contains(tag)
-                    then
-                        concatenatedTags = concatenatedTags..","..tag;
-                    end
-                end
-                returnString = returnString..concatenatedTags;
-            end
-        end
-    end
-    return returnString;
-end
-
-function TraitTags:getTagsStatistics()
+function TraitTags:getTagStatistics()
     local tempTable = {};
     local sortTable = {};
     local returnString = "";
@@ -154,4 +123,100 @@ function TraitTags:getTagsStatistics()
     end
 
     return returnString;
+end
+
+
+function TraitTags:getPlayerTraitTags(player)
+    local playerTraits = player:getTraits();
+    local concatenatedTags = "";
+    local returnString = "";
+
+    if playerTraits ~= nil
+    then
+        for i = 1, playerTraits:size() -1
+        do
+            local trait = playerTraits:get(i);
+
+            if TraitTags[trait] ~= nil
+            then
+                concatenatedTags = "";
+                for _,tag in pairs(TraitTags[trait])
+                do
+                    if not returnString:contains(tag)
+                    then
+                        concatenatedTags = concatenatedTags..","..tag;
+                    end
+                end
+                returnString = returnString..concatenatedTags;
+            end
+        end
+    end
+    returnString = returnString:sub(2, returnString:len())
+    return returnString;
+end
+
+
+function TraitTags:getPlayerTagStatistics(player)
+    local playerTraits = player:getTraits();
+    local trait = {};
+    local tagTable = {};
+    local tempTable = {};
+    local sortTable = {};
+    local returnString = "";
+
+    --get
+    for i=1, playerTraits:size() -1
+    do
+        trait = playerTraits:get(i);
+        if TraitTags[trait] ~= nil
+        then
+            tagTable = TraitTags[trait];
+            for _, tag in ipairs(tagTable)
+            do
+                if tempTable[tag] ~= nil
+                then
+                    tempTable[tag] = tempTable[tag] + 1;
+                else
+                    tempTable[tag] = 1
+                end
+            end
+        end
+    end
+
+    --sort
+    for key, value in pairs(tempTable) do
+        table.insert(sortTable, {tag = key, count = value})
+    end
+    table.sort(sortTable, function(a, b) return a.count > b.count end);
+
+    --condense (limitation: cannot make a sorted table in vanilla lua. Return as string instead.)
+
+    for _,entry in pairs(sortTable)
+    do
+        returnString = returnString..entry.tag..": "..entry.count.."; "
+    end
+
+    return returnString;
+end
+
+function TraitTags:PlayerTagLargerCountThan(player, subjectTag, comparatorTag)
+    local stats = TraitTags:getPlayerTagStatistics(player);
+    local subjectIndexS, subjectIndexE = stats:find(subjectTag);
+    local comparatorIndexS, comparatorIndexE = stats:find(comparatorTag);
+
+    if subjectIndexS ~= nil and comparatorIndexS ~= nil
+    then
+        if subjectIndexS > comparatorIndexS
+        then
+            return true;
+        else
+            return false;
+        end
+    else
+        if getDebug()
+        then
+            print("TraitTags: PlayerTagLargerCountThan(): One or more of the given tags is null");
+        end
+        return nil;
+    end
 end
